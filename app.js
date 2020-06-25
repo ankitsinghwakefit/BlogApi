9b114f43b34e7a28b4f4bd6c36be3bcb57125db9
Ankit Singh <ankit@wittypen.com>
2020-06-25 06:09:16 +0530

    CORS solved

--- app.js
+++ app.js
@@ -20,10 +20,7 @@ mongoose.connect(
 		console.log(err ? err : "Connected...");
 	}
 );
-// mongoose.connect('mongodb://localhost:27017/reddit',
-// { useNewUrlParser: true },()=>{
-//   console.log("DataBase connected...")
-// });
+
 
 var app = express();
 mongoose.set("useCreateIndex", true);
@@ -32,6 +29,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
 
 // Routes
 app.use("/api/v1/profiles", profilesRouter);
